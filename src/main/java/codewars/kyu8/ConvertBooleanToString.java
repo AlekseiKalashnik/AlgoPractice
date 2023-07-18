06486fa3f5c3c4f0299ba61c274c3e10fc44026c
AlekseiKalashnik <id50538996@gmail.com>
2023-07-18 19:57:47 +0300

    add more

---
+++ src/main/java/codewars/kyu8/ConvertBooleanToString.java
@@ -0,0 +1,11 @@
+package codewars.kyu8;
+
+public class ConvertBooleanToString {
+    public static String convert(boolean b){
+        return String.valueOf(b);
+      }
+
+    //return b ? "true" : "false";
+    //Implement a function which convert the given boolean value into its string representation.
+    //Only valid inputs will be given.
+}
