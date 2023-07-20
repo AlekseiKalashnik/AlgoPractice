da897908b6b49ac1106841d08bd93b41b7f94e4f
AlekseiKalashnik <id50538996@gmail.com>
2023-07-20 22:06:01 +0300

    add more

---
+++ src/main/java/codewars/kyu8/SimpleMultiplication.java
@@ -0,0 +1,9 @@
+package codewars.kyu8;
+
+public class SimpleMultiplication {
+    public static int simpleMultiplication (int n) {
+        return n % 2 == 0 ? n * 8 : n * 9;
+    }
+
+    //This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+}
