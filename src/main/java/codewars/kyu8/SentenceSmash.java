6d282f178b7a58969ee56be88ca70f4938e61bbf
AlekseiKalashnik <id50538996@gmail.com>
2023-07-28 19:29:56 +0300

    add more

---
+++ src/main/java/codewars/kyu8/SentenceSmash.java
@@ -0,0 +1,22 @@
+package codewars.kyu8;
+
+public class SentenceSmash {
+    public static String smash(String... words) {
+        return words != null ? String.join(" ", words) : null;
+    }
+    //public static String smash(String... words) {
+    //    StringBuilder sb = new StringBuilder();
+    //    for (String word : words)
+    //    {
+    //      sb.append(word + " ");
+    //    }
+    //    return sb.toString().trim();
+    //  }
+
+    //Write a function that takes an array of words and smashes them together into a sentence and returns the sentence.
+    //You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word.
+    //Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+    //
+    //Example
+    //['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+}
