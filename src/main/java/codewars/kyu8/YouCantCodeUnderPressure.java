8fc49eac7ad8b79755fc12b30ac02141b11cf576
AlekseiKalashnik <id50538996@gmail.com>
2023-07-31 19:19:16 +0300

    add more

---
+++ src/main/java/codewars/kyu8/YouCantCodeUnderPressure.java
@@ -0,0 +1,8 @@
+package codewars.kyu8;
+
+public class YouCantCodeUnderPressure {
+    public static int doubleInteger(int i) {
+        return i*2;
+    }
+    //Code as fast as you can! You need to double the integer and return it.
+}
