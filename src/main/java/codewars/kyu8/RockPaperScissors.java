a05a4e7d28be773282fc5fa4c9ee4a8ecf381dbb
AlekseiKalashnik <id50538996@gmail.com>
2023-08-02 20:08:50 +0300

    add more

--- src/main/java/codewars/kyu8/RockPaperScissors.java
+++ src/main/java/codewars/kyu8/RockPaperScissors.java
@@ -1,14 +1,16 @@
 package codewars.kyu8;
 
+import java.util.Objects;
+
 public class RockPaperScissors {
-    public static String rps(String p1, String p2) {
-        if(p1 == p2) return "Draw!";
-        int p = (p1 + p2).equals("scissorspaper")
-                ? (p1 + p2).equals("rockscissors")
-                ? (p1 + p2).equals("paperrock")
-                ? 1 : 2;
-        return "Player " + p + " won!";
-    }
+//    public static String rps(String p1, String p2) {
+////        if(Objects.equals(p1, p2)) return "Draw!";
+////        int p = (p1 + p2).equals("scissorspaper")
+////                ? (p1 + p2).equals("rockscissors")
+////                ? (p1 + p2).equals("paperrock")
+////                ? 1 : 2;
+////        return "Player " + p + " won!";
+//    }
 
     //Let's play! You have to return which player won! In case of a draw return Draw!.
     //
