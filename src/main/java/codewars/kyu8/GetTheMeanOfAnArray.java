d196cbba88602afe390220f2ea6829b2d137c033
AlekseiKalashnik <id50538996@gmail.com>
2023-08-02 20:09:44 +0300

    add more

---
+++ src/main/java/codewars/kyu8/GetTheMeanOfAnArray.java
@@ -0,0 +1,19 @@
+package codewars.kyu8;
+
+import java.util.stream.IntStream;
+
+public class GetTheMeanOfAnArray {
+    public static int getAverage(int[] marks){
+        return (IntStream.of(marks).sum())/marks.length;
+    }
+
+    //It's the academic year's end, fateful moment of your school report.
+    // The averages must be calculated.
+    // All the students come to you and entreat you to calculate their average for them.
+    // Easy !
+    // You just need to write a script.
+    //
+    //Return the average of the given array rounded down to its nearest integer.
+    //
+    //The array will never be empty.
+}
