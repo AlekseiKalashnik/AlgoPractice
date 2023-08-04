76bc49debdef04124e57b29dcf00c1a670a998e4
AlekseiKalashnik <id50538996@gmail.com>
2023-08-04 19:31:54 +0300

    add more

---
+++ src/main/java/codewars/kyu8/ThirdAngleOfTriangle.java
@@ -0,0 +1,12 @@
+package codewars.kyu8;
+
+public class ThirdAngleOfTriangle {
+    public static int otherAngle(int angle1, int angle2) {
+        return 180 - (angle1 + angle2);
+    }
+    //You are given two interior angles (in degrees) of a triangle.
+    //
+    //Write a function to return the 3rd.
+    //
+    //Note: only positive integers will be tested.
+}
