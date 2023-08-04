29d39283f6d538281a1567d13d71ffbc3285a17e
AlekseiKalashnik <id50538996@gmail.com>
2023-08-04 19:25:26 +0300

    add more

---
+++ src/main/java/codewars/kyu8/GrasshopperPersonalizedMessage.java
@@ -0,0 +1,15 @@
+package codewars.kyu8;
+
+public class GrasshopperPersonalizedMessage {
+    static String greet(String name, String owner) {
+        return name.equals(owner) ? "Hello boss" : "Hello guest";
+    }
+    //Create a function that gives a personalized greeting.
+    // This function takes two parameters: name and owner.
+    //
+    //Use conditionals to return the proper message:
+    //
+    //case return
+    //name equals owner 'Hello boss'
+    //otherwise 'Hello guest'
+}
