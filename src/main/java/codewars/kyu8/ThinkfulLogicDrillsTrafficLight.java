0cca3190bf604a2f67c5ae0e53af4b24ea2d7dfa
AlekseiKalashnik <id50538996@gmail.com>
2023-08-04 19:28:42 +0300

    add more

---
+++ src/main/java/codewars/kyu8/ThinkfulLogicDrillsTrafficLight.java
@@ -0,0 +1,17 @@
+package codewars.kyu8;
+
+public class ThinkfulLogicDrillsTrafficLight {
+    public static String updateLight(String current) {
+        return switch (current) {
+            case "red" -> "green";
+            case "yellow" -> "red";
+            case "green" -> "yellow";
+            default -> throw new IllegalArgumentException();
+        };
+    }
+    //You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+    //
+    //Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+    //
+    //For example, when the input is green, output should be yellow.
+}
