d092ba7a8e6656df2cfb5ec9991de58d14baf86f
AlekseiKalashnik <id50538996@gmail.com>
2023-08-09 21:34:19 +0300

    add more

---
+++ src/main/java/codewars/kyu8/VolumeOfCuboid.java
@@ -0,0 +1,9 @@
+package codewars.kyu8;
+
+public class VolumeOfCuboid {
+    public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+        return length * width * height;
+    }
+    //Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid.
+    // Write a function to help Bob with this calculation.
+}
