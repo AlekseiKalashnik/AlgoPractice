806317d991d1f3f278b0744f42e8cce95f74d3a1
AlekseiKalashnik <id50538996@gmail.com>
2023-08-11 20:10:15 +0300

    add more

---
+++ src/main/java/codewars/kyu8/GrasshopperMessiGoalsFunction.java
@@ -0,0 +1,19 @@
+package codewars.kyu8;
+
+public class GrasshopperMessiGoalsFunction {
+    public static int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+        return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+    }
+    //Messi is a soccer player with goals in three leagues:
+    //
+    //LaLiga
+    //Copa del Rey
+    //Champions
+    //Complete the function to return his total number of goals in all three leagues.
+    //
+    //Note: the input will always be valid.
+    //
+    //For example:
+    //
+    //5, 10, 2  -->  17
+}
