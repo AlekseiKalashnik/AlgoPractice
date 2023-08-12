8baae8be71d7caa6cbefa018e984ded720754cb0
AlekseiKalashnik <id50538996@gmail.com>
2023-08-12 12:33:30 +0300

    add more

---
+++ src/main/java/codewars/kyu8/GrasshopperDebugSayHello.java
@@ -0,0 +1,14 @@
+package codewars.kyu8;
+
+public class GrasshopperDebugSayHello {
+    public static String sayHello(String name) {
+
+        return "Hello, " + name;
+    }
+    //The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard.
+    // It is your job to fix the code and get the program working again!
+    //
+    //Example output:
+    //
+    //Hello, Mr. Spock
+}
