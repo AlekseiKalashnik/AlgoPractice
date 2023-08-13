00ef852520d324422bc0412450413d4f1d71a4a0
AlekseiKalashnik <id50538996@gmail.com>
2023-08-13 10:32:49 +0300

    add more

---
+++ src/main/java/codewars/kyu8/Function2SquaringArgument.java
@@ -0,0 +1,8 @@
+package codewars.kyu8;
+
+public class Function2SquaringArgument {
+    public static int square(int n) {
+        return n*n;
+    }
+    //Now you have to write a function that takes an argument and returns the square of it.
+}
