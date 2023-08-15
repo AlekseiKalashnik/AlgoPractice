63fceb5b0774ecfbec2ded205c0b2c3855bc8f9b
AlekseiKalashnik <id50538996@gmail.com>
2023-08-15 20:09:07 +0300

    add more

---
+++ src/main/java/codewars/kyu8/GetNthEvenNumber.java
@@ -0,0 +1,15 @@
+package codewars.kyu8;
+
+public class GetNthEvenNumber {
+    public static int nthEven(int n) {
+        return n * 2 - 2;
+    }
+    //Return the Nth Even Number
+    //
+    //Example(Input --> Output)
+    //
+    //1 --> 0 (the first even number is 0)
+    //3 --> 4 (the 3rd even number is 4 (0, 2, 4))
+    //100 --> 198
+    //1298734 --> 2597466
+}
