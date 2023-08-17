bff777a1f271b205138c3e2ace4bc23d6eff179d
AlekseiKalashnik <id50538996@gmail.com>
2023-08-17 18:43:40 +0300

    add more

---
+++ src/main/java/codewars/kyu8/GetCharacterFromASCIIValue.java
@@ -0,0 +1,14 @@
+package codewars.kyu8;
+
+public class GetCharacterFromASCIIValue {
+    public static char getChar(int c) {
+        return (char)c;
+    }
+    //Write a function which takes a number and returns the corresponding ASCII char for that value.
+    //
+    //Example:
+    //
+    //65 --> 'A'
+    //97 --> 'a'
+    //48 --> '0
+}
