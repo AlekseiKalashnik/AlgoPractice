0707fe1a0c21c2dc9f86fef33171206b58e2bdad
AlekseiKalashnik <id50538996@gmail.com>
2023-08-19 09:02:34 +0300

    add more

---
+++ src/main/java/codewars/kyu8/GrasshopperBasicFunctionFixer.java
@@ -0,0 +1,12 @@
+package codewars.kyu8;
+
+public class GrasshopperBasicFunctionFixer {
+    static int addFive(final int num) {
+        return num + 5;
+    }
+    //Fix the function
+    //I created this function to add five to any number that was passed in to it and return the new value.
+    // It doesn't throw any errors but it returns the wrong number.
+    //
+    //Can you help me fix the function?
+}
