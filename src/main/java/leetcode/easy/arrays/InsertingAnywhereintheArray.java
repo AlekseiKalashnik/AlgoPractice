2ffc5dc86e078f0cb2e8fd6a99b1b9254c2802c6
AlekseiKalashnik <id50538996@gmail.com>
2023-08-20 10:25:30 +0300

    add more

---
+++ src/main/java/leetcode/easy/arrays/InsertingAnywhereintheArray.java
@@ -0,0 +1,14 @@
+package leetcode.easy.arrays;
+
+public class InsertingAnywhereintheArray {
+    // Say we want to insert the element at index 2.
+// First, we will have to create space for the new element.
+    //TODO    for (int i = 4; i >= 2; i--) {
+        // Shift each element one position to the right.
+    //TODO       intArray[i + 1] = intArray[i];
+    //        }
+
+// Now that we have created space for the new element,
+// we can insert it at the required index.
+    //TODO    intArray[2] = 30;
+}
