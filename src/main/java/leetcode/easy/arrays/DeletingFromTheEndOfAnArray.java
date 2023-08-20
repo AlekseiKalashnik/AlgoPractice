e8eaf767e9017bf971a444d195213537bed6067e
AlekseiKalashnik <id50538996@gmail.com>
2023-08-20 10:48:34 +0300

    add more

---
+++ src/main/java/leetcode/easy/arrays/DeletingFromTheEndOfAnArray.java
@@ -0,0 +1,21 @@
+package leetcode.easy.arrays;
+
+public class DeletingFromTheEndOfAnArray {
+    //// Declare an integer array of 10 elements.
+    //int[] intArray = new int[10];
+    //
+    //// The array currently contains 0 elements
+    //int length = 0;
+    //
+    //// Add elements at the first 6 indexes of the Array.
+    //for(int i = 0; i < 6; i++) {
+    //    intArray[length] = i;
+    //    length++;
+    //}
+
+    /// Deletion from the end is as simple as reducing the length
+    //// of the array by 1.
+    //TODO length--;
+    //or
+    //TODO Arrays.copyOf(original, original.length-1)
+}
