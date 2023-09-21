dacdff976649a2f14f679e2949cefff76cf8f92c
AlekseiKalashnik <id50538996@gmail.com>
2023-09-21 20:39:19 +0300

    add more

--- src/main/java/leetcode/easy/arrays/ValidMountainArray.java
+++ src/main/java/leetcode/easy/arrays/ValidMountainArray.java
@@ -5,11 +5,11 @@ public class ValidMountainArray {
         int i = 0;
         int j = arr.length - 1;
         int n = arr.length - 1;
-        while (i + 1 < n && arr[i] < arr[i+1]) {
+        while (i + 1 < n && arr[i] < arr[i + 1]) {
             i++;
         }
 
-        while (j > 0 && arr[j] < arr[j-1]) {
+        while (j > 0 && arr[j] < arr[j - 1]) {
             j--;
         }
 
