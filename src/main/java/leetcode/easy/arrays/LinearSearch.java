5f43498a886e1d68a4c9b92f8ad6669522f50a41
AlekseiKalashnik <id50538996@gmail.com>
2023-09-22 17:03:31 +0300

    add more

--- src/main/java/leetcode/easy/arrays/LinearSearch.java
+++ src/main/java/leetcode/easy/arrays/LinearSearch.java
@@ -23,15 +23,15 @@ public class LinearSearch {
         return false;
     }
     /*
-    * If the index is not known, which is the case most of the time, then we can check every element in the Array.
-    * We continue checking elements until we find the element we're looking for, or we reach the end of the Array.
-    * This technique for finding an element by checking through all elements one by one is known as the linear search algorithm.
-    * In the worst case, a linear search ends up checking the entire Array. Therefore, the time complexity for a linear search is O(N).
-    * Let's see the linear search algorithm in action, with all the edge cases handled properly.
-    *
-    * When we say edge cases, we basically mean scenarios that you wouldn't expect to encounter.
-    * For example, the element you're searching for might not even exist in the Array.
-    * Or, an even rarer, but possible, scenario is that the input Array doesn't contain any elements at all, or perhaps it is null.
-    * It's important to handle all of these edge cases within the code.
-* */
+     * If the index is not known, which is the case most of the time, then we can check every element in the Array.
+     * We continue checking elements until we find the element we're looking for, or we reach the end of the Array.
+     * This technique for finding an element by checking through all elements one by one is known as the linear search algorithm.
+     * In the worst case, a linear search ends up checking the entire Array. Therefore, the time complexity for a linear search is O(N).
+     * Let's see the linear search algorithm in action, with all the edge cases handled properly.
+     *
+     * When we say edge cases, we basically mean scenarios that you wouldn't expect to encounter.
+     * For example, the element you're searching for might not even exist in the Array.
+     * Or, an even rarer, but possible, scenario is that the input Array doesn't contain any elements at all, or perhaps it is null.
+     * It's important to handle all of these edge cases within the code.
+     * */
 }
