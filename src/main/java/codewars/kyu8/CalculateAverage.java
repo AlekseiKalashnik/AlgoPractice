dce54bdf30843be0ec67ec2d0fe89ec42cb8c2d7
AlekseiKalashnik <id50538996@gmail.com>
2023-10-04 19:46:33 +0300

    add more

--- src/main/java/codewars/kyu8/CalculateAverage.java
+++ src/main/java/codewars/kyu8/CalculateAverage.java
@@ -3,7 +3,7 @@ package codewars.kyu8;
 import java.util.Arrays;
 
 public class CalculateAverage {
-    public static double find_average(int[] array){
+    public static double find_average(int[] array) {
         return Arrays.stream(array).average().orElse(0);
     }
 
