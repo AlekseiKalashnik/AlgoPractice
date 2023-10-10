535e6f4892ac81995b9366a8ac12e0d4118c7b28
AlekseiKalashnik <id50538996@gmail.com>
2023-10-10 19:29:01 +0300

    add more

--- src/main/java/codewars/kyu8/ExclusiveOrLogicalOperator.java
+++ src/main/java/codewars/kyu8/ExclusiveOrLogicalOperator.java
@@ -2,7 +2,7 @@ package codewars.kyu8;
 
 public class ExclusiveOrLogicalOperator {
     public static boolean xor(boolean a, boolean b) {
-        return a!=b;
+        return a != b;
     }
     //In some scripting languages like PHP, there exists a logical operator (e.g. &&, ||, and, or, etc.) called the "Exclusive Or" (hence the name of this Kata).
     // The exclusive or evaluates two booleans. It then returns true if exactly one of the two expressions are true, false otherwise. For example:
