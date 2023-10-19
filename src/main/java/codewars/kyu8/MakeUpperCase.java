1a8c7fd31c57abfd606676aef8330c5673059db9
AlekseiKalashnik <id50538996@gmail.com>
2023-10-19 21:16:56 +0300

    add more

--- src/main/java/codewars/kyu8/MakeUpperCase.java
+++ src/main/java/codewars/kyu8/MakeUpperCase.java
@@ -1,7 +1,7 @@
 package codewars.kyu8;
 
 public class MakeUpperCase {
-    public static String MakeUpperCase(String str){
+    public static String MakeUpperCase(String str) {
         return str.toUpperCase();
     }
 }
