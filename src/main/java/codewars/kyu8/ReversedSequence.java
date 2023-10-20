67f887043a04d04b99bba800d6ac08a9e9cd1f2e
AlekseiKalashnik <id50538996@gmail.com>
2023-10-20 11:02:19 +0300

    add more

--- src/main/java/codewars/kyu8/ReversedSequence.java
+++ src/main/java/codewars/kyu8/ReversedSequence.java
@@ -1,11 +1,11 @@
 package codewars.kyu8;
 
 public class ReversedSequence {
-    public static int[] reverse(int n){
+    public static int[] reverse(int n) {
         //your code
         int[] res = new int[n];
-        for (int i=0; i<n; i++)
-            res[i]=n-i;
+        for (int i = 0; i < n; i++)
+            res[i] = n - i;
         return res;
     }
     //Build a function that returns an array of integers from n to 1 where n>0.
