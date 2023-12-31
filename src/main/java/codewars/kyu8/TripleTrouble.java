6fb58d4585302d23ddf110494d2353870496aa93
AlekseiKalashnik <id50538996@gmail.com>
2023-12-31 12:55:59 +0300

    add more

---
+++ src/main/java/codewars/kyu8/TripleTrouble.java
@@ -0,0 +1,15 @@
+package codewars.kyu8;
+
+public class TripleTrouble {
+    public static void main(String[] args) {
+        System.out.println(tripleTrouble("this", "test", "lock"));
+    }
+
+    public static String tripleTrouble(String one, String two, String three) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < one.length(); i++) {
+            sb.append(one.charAt(i)).append(two.charAt(i)).append(three.charAt(i));
+        }
+        return sb.toString();
+    }
+}
